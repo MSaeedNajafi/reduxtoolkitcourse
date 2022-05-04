9ee3b6e83875808f1c6beee2f52ad4d97a0db71f
Saeed Najafi <saeednajafi@BS-LT-IOS-012.local>
2022-05-04 12:32:47 +0200

    adding unit test

--- src/math.spec.js
+++ src/math.spec.js
@@ -1,1 +1,20 @@
-it('first test', ()=>{})
+import { isEven } from "./math"
+
+
+describe('isEven', ()=> {
+
+    it('should return true if given an even number', () =>  {
+        // function under test
+        const result = isEven(2);
+        expect(result).toEqual(true)
+    })
+    
+    it('should return false is given an odd number', () =>  {
+        // function under test
+        const result = isEven(1);
+        expect(result).toEqual(false)
+    })
+
+})
+
+
